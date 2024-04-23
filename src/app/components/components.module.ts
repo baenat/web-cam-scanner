17ebe416e30c213d8ecb3ea6f6269ca6faf72f43
Baenat <bae.julian10@gmail.com>
2024-04-23 12:35:35 -0500

    Remove plugins not need

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WebcamModule } from 'ngx-webcam';
-import { CameraComponent } from './camera/camera.component';
 import { HeaderComponent } from './header/header.component';
-import { TesseractComponent } from './tesseract/tesseract.component';
 import { ScannerComponent } from './scanner/scanner.component';
 import { ZXingScannerModule } from '@zxing/ngx-scanner';
 import { DevicesListComponent } from './selects/devices-list/devices-list.component';
@@ -21,9 +19,7 @@ import { MsgPermissionComponent } from './msg-permission/msg-permission.componen
 
 @NgModule({
   declarations: [
-    CameraComponent,
     HeaderComponent,
-    TesseractComponent,
     ScannerComponent,
     DevicesListComponent,
     ResponseScanComponent,
@@ -44,9 +40,7 @@ import { MsgPermissionComponent } from './msg-permission/msg-permission.componen
     ClipboardModule
   ],
   exports: [
-    CameraComponent,
     HeaderComponent,
-    TesseractComponent,
     ScannerComponent,
     DevicesListComponent,
   ]
